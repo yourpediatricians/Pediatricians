d1ec23e9aeefac84a40af9544ce97d4466ef6719
nayalparas <nayalparas@gmail.com>
2024-04-24 14:45:36 +0530

    navbar change

--- frontend/src/components/Navigation/Navigation.component.jsx
+++ frontend/src/components/Navigation/Navigation.component.jsx
@@ -1,12 +1,13 @@
 import axios from 'axios'
-import { useNavigate } from 'react-router-dom'
-import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import { useAuth } from '../../contexts/AuthContext'
 import { Button, Container, Nav, Navbar } from 'react-bootstrap'
 
 function Navigation() {
   const navigate = useNavigate()
   const { curUser, updateCurUser } = useAuth()
+  const [expanded, setExpanded] = useState(false)
 
   const logout = async () => {
     const res = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/users/logout`)
@@ -17,11 +18,11 @@ function Navigation() {
   }
 
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary" expanded={expanded}>
       <Container>
         <Navbar.Brand as={Link} to="/">Wepediatrics</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
+        <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setExpanded(!expanded)} />
+        <Navbar.Collapse id="basic-navbar-nav" onClick={() => setExpanded(false)}>
           <Nav className="me-auto">
             <Nav.Link as={Link} to="/">Home</Nav.Link>
             {/* <Nav.Link href="/signup">Join Us</Nav.Link> */}
@@ -42,7 +43,7 @@ function Navigation() {
                 </>
                 :
                 <>
-                  <Link to='/login'><Button className='ms-3'>Log In</Button></Link>
+                  <Link to='/login'><Button className='ms-3 mb-2 mb-lg-0'>Log In</Button></Link>
                   <Link to='/signup'><Button className='ms-3'>Get Started</Button></Link>
                 </>
             }
