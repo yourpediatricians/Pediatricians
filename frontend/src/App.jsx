3a15f11a98e2819eab3d34dadbff8796ba39eb15
lakshaysoni <lakshaysoni1111@gmail.com>
2024-06-20 18:10:02 +0530

    policies added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,11 @@ import SignupSuccess from './components/SignupSuccess/SignupSuccess.component'
 import VerifyMail from './components/VerifyMail/VerifyMail.component'
 import JoinUs from './components/JoinUs/JoinUs.component'
 
+
 import './App.css'
+import RefundPolicy from './components/Footer/RefundPolicy'
+import PrivacyPolicy from './components/Footer/PrivacyPolicy'
+import TermsOfUse from './components/Footer/TermsOfUse'
 
 function App() {
   const { curUser, updateCurUser } = useAuth()
@@ -35,6 +39,10 @@ function App() {
           <Route path='signup' element={<Signup />} />
           <Route path='signup/success' element={<SignupSuccess />} />
           <Route path='verify' element={<VerifyMail />} />
+          <Route path='refund-policy' element={<RefundPolicy />} />
+          <Route path='privacy-policy' element={<PrivacyPolicy />} />
+          <Route path='termsofuse' element={<TermsOfUse />} />
+
         </Route>
       </Routes>
     </>
---
+++ frontend/src/components/Footer/RefundPolicy.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function RefundPolicy() {
+  return (
+    <div>
+      <h1>Refund Policy:</h1>
+      <p>Our refund policy means Satisfaction guaranteed</p>
+      <h2>Eligibility for Refunds:</h2>
+      <p>We offer refunds within 3 days of Subscription</p>
+      <h2>How to Request a Refund:</h2>
+      <p>To request a refund, simply email (info@wepediatrics.com). our customer support line and include your account’s email. You’ll receive a response within 24 hours (usually much less). Our customer support will ask why you’re requesting a refund, but will not block your refund.</p>
+      <h2>Contact Us:</h2>
+      <p>If you have any questions about our refund policy, please contact us at: info@wepediatrics.com</p>
+    </div>
+  )
+}
+
+export default RefundPolicy
