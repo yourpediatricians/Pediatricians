24a0857d75fc455a1e46dd55fc0a2a004965d56b
nayalparas <nayalparas@gmail.com>
2024-06-28 00:03:40 +0530

    change smtp port and secur

--- backend/utils/sendVerificationToken.js
+++ backend/utils/sendVerificationToken.js
@@ -5,13 +5,13 @@ module.exports.sendVerificationToken = (userInfo) => {
     const transporter = nodemailer.createTransport({
         host: "smtpout.secureserver.net",
         secure: true,
-        secureConnection: false, // TLS requires secureConnection to be false
-        tls: {
-            ciphers: 'SSLv3'
-        },
-        requireTLS: true,
-        port: 587,
-        debug: true,
+        // secureConnection: false, // TLS requires secureConnection to be false
+        // tls: {
+        //     ciphers: 'SSLv3'
+        // },
+        // requireTLS: true,
+        port: 465,
+        // debug: true,
         auth: {
             user: process.env.GODADDY_USER,
             pass: process.env.GODADDY_PASS
